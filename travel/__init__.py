ed5e6680130cee98f33d1fe0fc2a9f7a6e7e0f84
Jed-n11714221 <jedi97c@gmail.com>
2025-05-25 00:18:01 +1000

    added redirected and url_for to flask imports to implement PRG pattern

--- travel/__init__.py
+++ travel/__init__.py
@@ -4,6 +4,10 @@ from flask_bootstrap import Bootstrap5
 def create_app():
     app = Flask(__name__)
     
+    Bootstrap5(app)
+
+    app.secret_key = 'somerandomvalue'
+    
     # add Blueprints
     from . import views
     app.register_blueprint(views.mainbp)
