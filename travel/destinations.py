af65ec33597258b51c0efe3217c35a66bc56705e
Jed-n11714221 <jedi97c@gmail.com>
2025-05-27 21:02:18 +1000

    updated destinations.py to facilitate logged in users

--- travel/destinations.py
+++ travel/destinations.py
@@ -1,9 +1,10 @@
-from flask import Blueprint, render_template, request, redirect, url_for
+from flask import Blueprint, render_template, request, redirect, url_for, flash, abort
 from .models import Destination, Comment
 from .forms import DestinationForm, CommentForm
 from . import db
 import os
 from werkzeug.utils import secure_filename
+from flask_login import login_required, current_user
 
 # Use of blue print to group routes, 
 # name - first argument is the blue print name 
@@ -13,24 +14,25 @@ destbp = Blueprint('destination', __name__, url_prefix='/destinations')
 @destbp.route('/<id>')
 def show(id):
     destination = db.session.scalar(db.select(Destination).where(Destination.id==id))
-    cform = CommentForm()
-    return render_template('destinations/show.html', destination=destination, form=cform)
+    form = CommentForm()
+    if not destination:
+       abort(404)
+    return render_template('destinations/show.html', destination=destination, form=form)
 
 @destbp.route('/create', methods = ['GET', 'POST'])
+@login_required
 def create():
   print('Method type: ', request.method)
   form = DestinationForm()
   if form.validate_on_submit():
     db_file_path = check_upload_file(form)
-    destination = Destination(name=form.name.data,
-    description=form.description.data,
-    image = db_file_path,
-    currency=form.currency.data)
+    destination = Destination(name=form.name.data,description=form.description.data, 
+    image=db_file_path,currency=form.currency.data)
     # add the object to the db session
     db.session.add(destination)
     # commit to the database
     db.session.commit()
-    print('Successfully created new travel destination', 'success')
+    flash('Successfully created new travel destination', 'success')
     # Always end with redirect when form is valid
     return redirect(url_for('destination.create'))
   return render_template('destinations/create.html', form=form)
@@ -57,13 +59,13 @@ def comment(id):
     if form.validate_on_submit():  
       # read the comment from the form, associate the Comment's destination field
       # with the destination object from the above DB query
-      comment = Comment(text=form.text.data, destination=destination) 
+      comment = Comment(text=form.text.data, destination=destination, user=current_user) 
       # here the back-referencing works - comment.destination is set
       # and the link is created
       db.session.add(comment) 
       db.session.commit() 
       # flashing a message which needs to be handled by the html
       # flash('Your comment has been added', 'success')  
-      print('Your comment has been added', 'success') 
+      flash('Your comment has been added', 'success') 
     # using redirect sends a GET request to destination.show
     return redirect(url_for('destination.show', id=id))
