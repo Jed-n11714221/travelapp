1906b4cbf3250c1611ebdfd8f42db689cc6d33aa
Jed-n11714221 <jedi97c@gmail.com>
2025-05-27 20:22:32 +1000

    added file validators for image upload on destination form

--- travel/forms.py
+++ travel/forms.py
@@ -1,13 +1,18 @@
 from flask_wtf import FlaskForm
 from wtforms.fields import TextAreaField,SubmitField, StringField, PasswordField
 from wtforms.validators import InputRequired, Email, EqualTo, Length
+from flask_wtf.file import FileRequired, FileField, FileAllowed
+
+ALLOWED_FILE = {'PNG','JPG','png','jpg'}
 
 class DestinationForm(FlaskForm):
   name = StringField('Country', validators=[InputRequired()])
   # adding two validators, one to ensure input is entered and other to check if the 
   # description meets the length requirements
   description = TextAreaField('Description', validators = [InputRequired()])
-  image = StringField('Cover Image', validators=[InputRequired()])
+  image = FileField('Destination Image', validators=[
+    FileRequired(message = 'Image cannot be empty'),
+    FileAllowed(ALLOWED_FILE, message='Only supports png, jpg, JPG, PNG')])
   currency = StringField('Currency', validators=[InputRequired()])
   submit = SubmitField("Create")
 
