0f41662fa9122f01c575aa9a39b6aab5b999544b
Jed-n11714221 <jedi97c@gmail.com>
2025-05-27 20:50:39 +1000

    initialised flask-login login_manager in init.py

--- travel/__init__.py
+++ travel/__init__.py
@@ -1,6 +1,9 @@
-from flask import Flask
+from flask import Flask, render_template
 from flask_bootstrap import Bootstrap5
 from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager
+from flask_bcrypt import Bcrypt
+import datetime
 
 db = SQLAlchemy()
 
@@ -17,11 +20,33 @@ def create_app():
     UPLOAD_FOLDER = '/static/image'
     app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
+    login_manager = LoginManager()
+    login_manager.login_view = 'auth.login'
+    login_manager.init_app(app)
+
+    # create a user loader function takes userid and returns User
+    from .models import User  # importing here to avoid circular references
+    @login_manager.user_loader
+    def load_user(user_id):
+        return db.session.scalar(db.select(User).where(User.id==user_id))
+
     from . import views
     app.register_blueprint(views.mainbp)
     from . import destinations
     app.register_blueprint(destinations.destbp)
     from . import auth
     app.register_blueprint(auth.authbp)
+
+    @app.errorhandler(404) 
+    # inbuilt function which takes error as parameter 
+    def not_found(e): 
+      return render_template("404.html", error=e)
+    
+    # this creates a dictionary of variables that are available
+    # to all html templates
+    @app.context_processor
+    def get_context():
+      year = datetime.datetime.today().year
+      return dict(year=year)
 
     return app
