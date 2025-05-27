c23dd95bd03058e4adfed1f1ae6d12f5263b42a4
Jed-n11714221 <jedi97c@gmail.com>
2025-05-27 20:26:49 +1000

    added check upload file function and updated destination create form to store the image file name

--- travel/destinations.py
+++ travel/destinations.py
@@ -2,6 +2,8 @@ from flask import Blueprint, render_template, request, redirect, url_for
 from .models import Destination, Comment
 from .forms import DestinationForm, CommentForm
 from . import db
+import os
+from werkzeug.utils import secure_filename
 
 # Use of blue print to group routes, 
 # name - first argument is the blue print name 
@@ -19,9 +21,10 @@ def create():
   print('Method type: ', request.method)
   form = DestinationForm()
   if form.validate_on_submit():
+    db_file_path = check_upload_file(form)
     destination = Destination(name=form.name.data,
     description=form.description.data,
-    image=form.image.data,
+    image = db_file_path,
     currency=form.currency.data)
     # add the object to the db session
     db.session.add(destination)
@@ -31,6 +34,20 @@ def create():
     # Always end with redirect when form is valid
     return redirect(url_for('destination.create'))
   return render_template('destinations/create.html', form=form)
+
+def check_upload_file(form):
+  # get file data from form  
+  fp = form.image.data
+  filename = fp.filename
+  # get the current path of the module file… store image file relative to this path  
+  BASE_PATH = os.path.dirname(__file__)
+  # upload file location – directory of this file/static/image
+  upload_path = os.path.join(BASE_PATH,'static/image',secure_filename(filename))
+  # store relative path in DB as image location in HTML is relative
+  db_upload_path = '/static/image/' + secure_filename(filename)
+  # save the file and return the db upload path  
+  fp.save(upload_path)
+  return db_upload_path
 
 @destbp.route('/<id>/comment', methods=['GET', 'POST'])  
 def comment(id):  
